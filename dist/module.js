891b5dc0d3b781630d7bba9c87d4f9b32a15e3bb
Matt Carter <m@ttcarter.com>
2022-11-23 14:17:44 +1000

    task: v1.0.0

--- dist/module.js
+++ dist/module.js
@@ -1,5 +1,18 @@
 var $ddc53a4b3d7d8296$exports = {};
-window.embedGDoc = function embedGdoc(options) {
+/**
+* Embed a Google-Docs document within a webpage
+* @param {Object} options Options to configure behaviour
+* @param {string|HTMLElement} options.selector Either the DOM node to replace or a selector to use
+* @param {string} options.url The Google Docs published URL to embed - this generally ends in `/pub?embedded=true`
+* @param {Object} [options.urlOptions] Additional Fetch options when retrieving the document from the `url`
+* @param {boolean} [options.fixContentTrim=true] Remove the outer wrapping of the element and just use the embedded content
+* @param {boolean} [options.fixWidth=true] Remove page width restrictions
+* @param {boolean} [options.fixPadding=true] Remove page padding
+* @param {boolean} [options.fixLinkTargets=true] Make all links open in a new tab instead of replacing the current one
+* @param {boolean} [options.fixLinkShorten=true] Remove Google tracking URL prefix from links
+* @param {boolean} [options.fixImageTitleAsLink=true] If an image "alternative text" (actually the `title` attribute) looks like a link make the image linkable - this is to fix how Google Docs weirdly handles image linking
+*
+*/ window.embedGDoc = function embedGdoc(options) {
     let settings = {
         selector: null,
         url: null,
@@ -9,20 +22,23 @@ window.embedGDoc = function embedGdoc(options) {
                 "Accept": "text/html"
             }
         },
+        fixContentTrim: true,
         fixWidth: true,
         fixPadding: true,
         fixLinkTargets: true,
         fixLinkShorten: true,
+        fixImageTitleAsLink: true,
         ...options
     };
     if (!settings.selector) throw new Error("Selector must be specified");
     if (!settings.url) throw new Error("URL must be specified");
-    let embedEl = document.querySelector(settings.selector);
+    let embedEl = typeof settings.selector == "string" ? document.querySelector(settings.selector) : settings.selector;
     if (!embedEl) throw new Error(`Cannot find selector "${settings.selector} to embed GDoc`);
     return fetch(settings.url, settings.urlOptions).then((res)=>res.text()).then((html)=>{
         let doc = document.createElement("div");
         doc.setHTML(html); // Splat HTML into temporary div
-        doc = doc.querySelector(".doc-content"); // Narrow down to just the contents
+        if (settings.fixContentTrim) // Narrow down to just the contents
+        doc = doc.querySelector(".doc-content");
         // fixWidth and/or fixPadding {{{
         if (settings.fixWidth || settings.fixPadding) doc.querySelector("div").setAttribute("style", [
             settings.fixWidth && "max-width: none",
@@ -42,6 +58,18 @@ window.embedGDoc = function embedGdoc(options) {
                 if (settings.fixLinkShorten) el.setAttribute("href", el.getAttribute("href").replace(/^https:\/\/www\.google\.com\/url\?q=(.*?)&.*$/, "$1") // Remove `&...` slush from GitHub URLs
                 .replace(/^https:\/\/www\.google\.com\/url\?q=/, "") // Rewrite all other URLs
                 );
+            });
+        }
+        // }}}
+        // fixImageTitleAsLink {{{
+        if (settings.fixImageTitleAsLink) {
+            var _doc$querySelectorAll2;
+            _doc$querySelectorAll2 = doc.querySelectorAll("img[title]"), Array.from(_doc$querySelectorAll2).filter((el)=>/^https?:\/\//.test(el.getAttribute("title"))).forEach((el)=>{
+                let wrapper = document.createElement("a");
+                wrapper.setAttribute("href", el.getAttribute("title"));
+                if (settings.fixLinkTargets) wrapper.setAttribute("target", "_blank");
+                el.replaceWith(wrapper);
+                wrapper.replaceChildren(el);
             });
         }
         // }}}
